96d9a91e926b72f4106ef2b3b8d916616ab86683
Pavel Gavrilov <pavgavrilov@gmail.com>
2013-04-01 12:56:10 +0300

    Done with static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_url = 'Ruby on Rails Tutorial | '
+    if @title.nil?
+      return base_url
+    else
+      return base_url + @title
+    end
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @attr = 'Ruby on Rails Tutorial | '
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector('title',
+                      content: @attr + 'Home')
     end
   end
 
@@ -14,12 +25,37 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it 'should have the right title' do
+      get 'contact'
+      response.should have_selector('title',
+                      content: @attr + 'Contact')
+    end
   end
 
   describe "GET 'help'" do
     it "returns http success" do
       get 'help'
       response.should be_success
+    end
+
+    it 'should have the right title' do
+      get 'help'
+      response.should have_selector('title',
+                      content: @attr + 'Help')
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it 'should return the right title' do
+      get 'about'
+      response.should have_selector('title',
+                      content: @attr + 'About')
     end
   end
 
