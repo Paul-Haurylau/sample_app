567d3ef082291ad3ac9d84edbf144b49d659e3ca
Pavel Gavrilov <pavgavrilov@gmail.com>
2013-04-02 19:56:31 +0300

    User signup complete

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Users" do
+
+  #describe 'Sign up' do
+  #  describe 'failure' do
+  #    it 'should not create a new user' do
+  #      visit signup_path
+  #      fill_in 'Name', with: ''
+  #      fill_in 'Email', with: ''
+  #      fill_in 'Password', with: ''
+  #      fill_in 'Confirmation', with: ''
+  #      click_button
+  #
+  #    end
+  #  end
+  #end
+end
